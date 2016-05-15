4ae2113340093241b2d16fe495fde7456ce3f343
runnerdave <runnerdave@gmail.com>
2016-05-15 16:20:03 +1000

    setup logger and maven setup with dependencies

--- src/main/java/net/runnerdave/App.java
+++ src/main/java/net/runnerdave/App.java
@@ -1,13 +1,47 @@
 package net.runnerdave;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 /**
- * Hello world!
+ * Hibernate setup harness.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	private static final Logger LOGGER = Logger.getLogger("hibernate template");
+
+	public static void main(String[] args) {
+		System.out.println("Hibernate setup 6");
+		LOGGER.info("logger working");
+	}
+
+	public void run() {
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			session = sessionFactory.openSession();
+			//persistPerson(session);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, e.getMessage(), e);
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+			if (sessionFactory != null) {
+				sessionFactory.close();
+			}
+		}
+	}
 }
